99ba4dfa0d99e0185da5057d6666f2af368f385a
victor1338 <48907925+victor1338@users.noreply.github.com>
2022-11-19 23:07:13 +0800

    2 files modified

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <Button variant="primary">Primary</Button>{' '}
+        <Button
+          variant="primary"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </Button>
       </header>
     </div>
   );
